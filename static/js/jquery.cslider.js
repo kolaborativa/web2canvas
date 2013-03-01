8e7e3ed4ff05e8b472ae5e42a8a3ce51a91b9790
gustavox4ids <gustavo@x4ids.com.br>
2013-03-01 16:51:29 -0300

    novo slider na home

--- static/js/jquery.cslider.js
+++ static/js/jquery.cslider.js
@@ -15,7 +15,7 @@
 		current		: 0, 	// index of current slide
 		bgincrement	: 50,	// increment the bg position (parallax effect) when sliding
 		autoplay	: false,// slideshow on / off
-		interval	: 4000  // time between transitions
+		interval	: 8000  // time between transitions
     };
 	
 	$.Slider.prototype 	= {
