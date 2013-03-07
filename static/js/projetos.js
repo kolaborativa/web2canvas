d91e42c7368fc63f4777b18189138d6fc3e57d36
R4bugento <diego@x4ids.com.br>
2013-03-07 00:02:00 -0300

    chamando funcao de excluir projeto do front-end (pagina de projetos)

--- static/js/projetos.js
+++ static/js/projetos.js
@@ -6,3 +6,18 @@ $(document).ready(function () {
 $(".btn_file").on('click', function() {
    $('.input_file').click();
 });
+
+/*
+=====================
+ EXCLUIR PROJETO
+=====================
+*/
+
+$(".excluir_projeto").click(function() {
+  var mensagem_confirma = 'Tem certeza que deseja excluir esse projeto?';
+  if(confirm(mensagem_confirma)) {
+    var id_projeto = this.id;
+    var url = urlExcluiProjeto+id_projeto;
+    window.location=url;
+  }  
+});
